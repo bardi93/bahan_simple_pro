3e6b71506f70c6614529e249ad7bc2ffe0fca488
bardi93 <44915994+bardi93@users.noreply.github.com>
2021-03-03 15:01:24 +0700

    Add files via upload

--- bahan.py
+++ bahan.py
@@ -2,26 +2,26 @@
         if op.type in [13, 124]:
             if mid in op.param3:
                 if wait["autoJoin"] == True:
-                    if op.param2 in owner or op.param2 in bot:
+                    if op.param2 in creator or op.param2 in botmid:
                         cl.acceptChatInvitation(op.param1)
             if c1mid in op.param3:
                 if wait["autoJoin"] == True:
-                    if op.param2 in owner or op.param2 in bot:
+                    if op.param2 in creator or op.param2 in botmid:
                         c1.acceptChatInvitation(op.param1)
             if c2mid in op.param3:
                 if wait["autoJoin"] == True:
-                    if op.param2 in owner or op.param2 in bot:
+                    if op.param2 in creator or op.param2 in botmid:
                         c2.acceptChatInvitation(op.param1)
 #=======Pro__Invite===========
         if op.type in [13, 124]:
-            if op.param1 in protectinvite:
-                if op.param2 not in creator or bot:
+            if op.param1 in proInvite:
+                if op.param2 not in creator or botmid:
                     wait["blacklist"][op.param2] = True
                     for invite in op.param3.split('\x1e'):
-                        if invite not in creator or bot:
+                        if invite not in creator or botmid:
                             wait["blacklist"][invite] = True
-                            random.choice(botmid).cancelChatInvitation(op.param1, [invite])
-                            random.choice(botmid).deleteOtherFromChat(op.param1, [op.param2])
+                            random.choice(bot).cancelChatInvitation(op.param1, [invite])
+                            random.choice(bot).deleteOtherFromChat(op.param1, [op.param2])
 #===========================
 #Cmd proInvite
 
